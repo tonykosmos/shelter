955a387880cbe961de994d359ea002cf0988dc4c
tonykosmos <43154764+tonykosmos@users.noreply.github.com>
2021-02-27 07:54:24 +0300

    Update script.js

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,158 +1,190 @@
+let bodyImg = document.querySelector("body");
+
 let pets = [];
 const request = new XMLHttpRequest();
 request.open('GET', './pets.json');
 
+
 request.onload = () => {
   pets = JSON.parse(request.response);
 };
+  
+let menuIcon = document.querySelector(".burger-menu-icon");
+let burgerMenu = document.querySelector(".burger-menu");
+let mainLogo = document.querySelector(".logo");
+let shadow = document.querySelector(".shadow");
+let body = document.querySelector("body");
+let startLink = document.querySelector(".burger-menu .header-nav-list__item:first-child a");
+menuIcon.addEventListener("click", () => {
+    if (!menuIcon.classList.contains("menu-open") && !menuIcon.classList.contains("menu-close")) {
+        menuIcon.classList.add("menu-open");
+        burgerMenu.classList.add("burger-menu_visible");
+        shadow.classList.add("visible");
+        body.classList.add("no-scroll");
+    }
+    else if (menuIcon.classList.contains("menu-open")) {
+        closeBurger();
+    }
+    else {
+        menuIcon.classList.remove("menu-close");
+        menuIcon.classList.add("menu-open"); 
+        burgerMenu.classList.add("burger-menu_visible");
+        burgerMenu.classList.remove("burger-menu_non-visible");
+        shadow.classList.add("visible");
+        body.classList.add("no-scroll");
+    }
+});
 
-const menuIcon = document.querySelector('.burger-menu-icon');
-const burgerMenu = document.querySelector('.burger-menu');
-const shadow = document.querySelector('.shadow');
-const body = document.querySelector('body');
-const startLink = document.querySelector('.burger-menu .header-nav-list__item:first-child a');
-const modal = document.querySelector('.modal');
-const modalBody = document.querySelector('.modal-body');
-const modalCloseButton = document.querySelector('.modal button');
-const sliderItemImage = document.querySelector('.modal-image img');
-const sliderItemPetName = document.querySelector('.pet-name__heading');
-const sliderItemBreed = document.querySelector('.pet-name__subheading');
-const sliderItemPetDescribing = document.querySelector('.pet-describing');
-const sliderItemPetAge = document.querySelector('.pet-features__item:nth-child(1) span');
-const sliderItemPetInoculations = document.querySelector('.pet-features__item:nth-child(2) span');
-const sliderItemPetDiseases = document.querySelector('.pet-features__item:nth-child(3) span');
-const sliderItemPetParasites = document.querySelector('.pet-features__item:nth-child(4) span');
-const sliderItems = document.querySelectorAll('.slider-active .slider-item');
-const hideSliderItems = document.querySelectorAll('.not-active .slider-item');
-const items = document.querySelectorAll('.slider-items-block');
-const leftArrow = document.querySelector('.arrow-left');
-const rightArrow = document.querySelector('.arrow-right');
+startLink.addEventListener("click", closeBurger);
 
-const set = new Set();
-let arr = [4, 0, 2];
 
-function closeBurger() {
-  if (menuIcon.classList.contains('menu-open')) {
-    menuIcon.classList.add('menu-close');
-    menuIcon.classList.remove('menu-open');
-    burgerMenu.classList.remove('burger-menu_visible');
-    burgerMenu.classList.add('burger-menu_non-visible');
-    shadow.classList.remove('visible');
-    body.classList.remove('no-scroll');
-  }
+function closeBurger(){
+    if (menuIcon.classList.contains("menu-open")) {
+        menuIcon.classList.add("menu-close");
+        menuIcon.classList.remove("menu-open");
+        burgerMenu.classList.remove("burger-menu_visible");
+        burgerMenu.classList.add("burger-menu_non-visible");
+        shadow.classList.remove("visible");
+        body.classList.remove("no-scroll");
+    }
 }
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-startLink.addEventListener('click', closeBurger);
-
-menuIcon.addEventListener('click', () => {
-  if (!menuIcon.classList.contains('menu-open') && !menuIcon.classList.contains('menu-close')) {
-    menuIcon.classList.add('menu-open');
-    burgerMenu.classList.add('burger-menu_visible');
-    shadow.classList.add('visible');
-    body.classList.add('no-scroll');
-  } else if (menuIcon.classList.contains('menu-open')) {
-    closeBurger();
-  } else {
-    menuIcon.classList.remove('menu-close');
-    menuIcon.classList.add('menu-open');
-    burgerMenu.classList.add('burger-menu_visible');
-    burgerMenu.classList.remove('burger-menu_non-visible');
-    shadow.classList.add('visible');
-    body.classList.add('no-scroll');
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
   }
-});
+
+
+let left = document.querySelectorAll(".arrow-left");
+let right = document.querySelectorAll(".arrow-right");
+
+let set = new Set();
+let arr = [4, 0, 2];
 
 function changeSlide() {
-  const petName = document.querySelectorAll('.not-active .slider-item__name');
-  const sliderImg = document.querySelectorAll('.not-active .slider-item__image img');
-  while (set.size < 6) {
-    set.add(getRandomInt(0, pets.length - 1));
-  }
-
-  for (const value of set) {
-    if (value === arr[0] || value === arr[1] || value === arr[2]) {
-      set.delete(value);
+  console.log(sliderItems);
+  let petName = document.querySelectorAll(".not-active .slider-item__name");
+  let sliderImg = document.querySelectorAll(".not-active .slider-item__image img");
+    while (set.size < 6) {
+        set.add(getRandomInt(0,pets.length - 1));
     }
-  }
-
+    for (let value of set) {
+      if (value == arr[0] || value == arr[1] || value == arr[2]) {
+      set.delete(value);
+   }
+  }  
   arr = Array.from(set);
   arr.length = 3;
-
-  for (let i = 0; i < petName.length; i += 1) {
-    petName[i].textContent = pets[arr[i]].name;
-    sliderImg[i].src = pets[arr[i]].img;
-  }
-
-  set.clear();
-}
-
-leftArrow.addEventListener('click', changeSlide);
-rightArrow.addEventListener('click', changeSlide);
-
-for (let i = 0; i < sliderItems.length; i += 1) {
-  sliderItems[i].addEventListener('click', () => {
-    if (!modal.classList.contains('active')) {
-      modal.classList.add('active');
-      shadow.classList.add('visible');
-      body.classList.add('no-scroll');
-      sliderItemImage.src = pets[arr[i]].img;
-      sliderItemPetName.textContent = pets[arr[i]].name;
-      sliderItemBreed.textContent = pets[arr[i]].type;
-      sliderItemPetDescribing.textContent = pets[arr[i]].description;
-      sliderItemPetAge.textContent = pets[arr[i]].age;
-      sliderItemPetInoculations.textContent = pets[arr[i]].inoculations;
-      sliderItemPetDiseases.textContent = pets[arr[i]].diseases;
-      sliderItemPetParasites.textContent = pets[arr[i]].parasites;
+  console.log(arr);
+    for (let i = 0; i < petName.length; i++) { 
+        petName[i].textContent = pets[arr[i]].name;
+        sliderImg[i].src = pets[arr[i]].img;
     }
-  });
+    set.clear();
 }
 
-for (let i = 0; i < hideSliderItems.length; i += 1) {
-  hideSliderItems[i].addEventListener('click', () => {
-    if (!modal.classList.contains('active')) {
-      modal.classList.add('active');
-      shadow.classList.add('visible');
-      body.classList.add('no-scroll');
-      sliderItemImage.src = pets[arr[i]].img;
-      sliderItemPetName.textContent = pets[arr[i]].name;
-      sliderItemBreed.textContent = pets[arr[i]].type;
-      sliderItemPetDescribing.textContent = pets[arr[i]].description;
-      sliderItemPetAge.textContent = pets[arr[i]].age;
-      sliderItemPetInoculations.textContent = pets[arr[i]].inoculations;
-      sliderItemPetDiseases.textContent = pets[arr[i]].diseases;
-      sliderItemPetParasites.textContent = pets[arr[i]].parasites;
-    }
-  });
+left.forEach( (elem) => elem.addEventListener("click", changeSlide));
+right.forEach( (elem) => elem.addEventListener("click", changeSlide));
+
+let modal = document.querySelector(".modal");
+let modalBody = document.querySelector(".modal-body");
+let modalCloseButton = document.querySelector(".modal button");
+
+let sliderItemImage = document.querySelector(".modal-image img");
+let sliderItemPetName = document.querySelector(".pet-name__heading");
+let sliderItemBreed = document.querySelector(".pet-name__subheading");
+let sliderItemPetDescribing = document.querySelector(".pet-describing");
+let sliderItemPetAge = document.querySelector(".pet-features__item:nth-child(1) span");
+let sliderItemPetInoculations = document.querySelector(".pet-features__item:nth-child(2) span");
+let sliderItemPetDiseases = document.querySelector(".pet-features__item:nth-child(3) span");
+let sliderItemPetParasites = document.querySelector(".pet-features__item:nth-child(4) span");
+
+let sliderItems =  document.querySelectorAll(".slider-active .slider-item");
+let hideSliderItems = document.querySelectorAll(".not-active .slider-item");
+
+for (let i = 0; i < sliderItems.length; i++) {
+  sliderItems[i].addEventListener("click", () => {
+      if (!modal.classList.contains("active")) {
+          modal.classList.add("active");
+          shadow.classList.add("visible");
+          body.classList.add("no-scroll");
+          sliderItemImage.src = pets[arr[i]].img;
+          sliderItemPetName.textContent = pets[arr[i]].name;
+          sliderItemBreed.textContent = pets[arr[i]].type;
+          sliderItemPetDescribing.textContent = pets[arr[i]].description;
+          sliderItemPetAge.textContent = pets[arr[i]].age;
+          sliderItemPetInoculations.textContent = pets[arr[i]].inoculations;
+          sliderItemPetDiseases.textContent = pets[arr[i]].diseases;
+          sliderItemPetParasites.textContent = pets[arr[i]].parasites;
+
+      }
+  })
+} 
+
+for (let i = 0; i < hideSliderItems.length; i++) {
+  hideSliderItems[i].addEventListener("click", () => {
+      if (!modal.classList.contains("active")) {
+          modal.classList.add("active");
+          shadow.classList.add("visible");
+          body.classList.add("no-scroll");
+          sliderItemImage.src = pets[arr[i]].img;
+          sliderItemPetName.textContent = pets[arr[i]].name;
+          sliderItemBreed.textContent = pets[arr[i]].type;
+          sliderItemPetDescribing.textContent = pets[arr[i]].description;
+          sliderItemPetAge.textContent = pets[arr[i]].age;
+          sliderItemPetInoculations.textContent = pets[arr[i]].inoculations;
+          sliderItemPetDiseases.textContent = pets[arr[i]].diseases;
+          sliderItemPetParasites.textContent = pets[arr[i]].parasites;
+
+      }
+  })
+} 
+
+function changeModalContent() {
+  if (!modal.classList.contains("active")) {
+    modal.classList.add("active");
+    shadow.classList.add("visible");
+    body.classList.add("no-scroll");
+    sliderItemImage.src = pets[arr[i]].img;
+    sliderItemPetName.textContent = pets[arr[i]].name;
+    sliderItemBreed.textContent = pets[arr[i]].type;
+    sliderItemPetDescribing.textContent = pets[arr[i]].description;
+    sliderItemPetAge.textContent = pets[arr[i]].age;
+    sliderItemPetInoculations.textContent = pets[arr[i]].inoculations;
+    sliderItemPetDiseases.textContent = pets[arr[i]].diseases;
+    sliderItemPetParasites.textContent = pets[arr[i]].parasites;
+
 }
+}
+
+
+request.send();
 
 function closeModal() {
-  if (modal.classList.contains('active')) {
-    modal.classList.remove('active');
-    shadow.classList.remove('visible');
-    body.classList.remove('no-scroll');
-  }
+    if (modal.classList.contains("active")) {
+        modal.classList.remove("active");
+        shadow.classList.remove("visible");
+        body.classList.remove("no-scroll");
+    } 
 }
 
-shadow.addEventListener('click', closeModal);
-shadow.addEventListener('click', closeBurger);
-modalCloseButton.addEventListener('click', closeModal);
+shadow.addEventListener("click", closeModal);
+shadow.addEventListener("click", closeBurger);
+modalCloseButton.addEventListener("click", closeModal);
 
-modalBody.addEventListener('mouseout', () => {
-  modalCloseButton.classList.add('button-background');
-});
+modalBody.addEventListener("mouseout", () => {
+  modalCloseButton.classList.add("button-background");
+})
 
-modalBody.addEventListener('mouseover', () => {
-  modalCloseButton.classList.remove('button-background');
-});
+modalBody.addEventListener("mouseover", () => {
+  modalCloseButton.classList.remove("button-background");
+})
 
-// slider
 
+// slider 
+
+let items = document.querySelectorAll('.slider-items-block');
 let currentItem = 0;
 let isEnabled = true;
 
@@ -202,5 +234,3 @@ document.querySelector('.arrow-right').addEventListener('click', function() {
 		nextItem(currentItem);
 	}
 });
-
-request.send();
